fda22ac35374407f23f5a6311f2a94eefb8a19b9
abeauquel <beauquel.alexandre@gmail.com>
2018-10-17 11:29:04 -0400

    Style (Veille) : Loader pour la timeline twitter

--- src/pages/veille.js
+++ src/pages/veille.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Timeline } from 'react-twitter-widgets'
+import Loader from 'react-loader-spinner'
 
 class Veille extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      isChargedTimeline:false,
+    }
+  }
 
   render() {
+
     return (
       <div>
         <p>Ma veille technologique, elle regroupe tous les articles que j'ai trouvé interessant sur twitter.</p>
-
-        <div >
+        {this.state.isChargedTimeline ? null:
+          <div >
+            <p>Chargement de la timeline twitter</p>
+            <Loader
+              type="Plane"
+              color="#00BFFF"
+              height="300"
+              width="300"
+            />
+          </div>}
+        <div className={this.state.isChargedTimeline ? '' : 'hide'}>
           <Timeline
+            key={'timeline'}
+            ref={'timeline'}
             dataSource={{
               sourceType: 'profile',
               screenName: 'alexbeauquel'
@@ -18,8 +38,17 @@ class Veille extends React.Component {
               username: 'alexbeauquel',
               height: '80vh'
             }}
-            onLoad={() => console.log('Timeline is loaded!')}
+            onLoad={() =>
+            {
+              console.log('Timeline is loaded!');
+              if (!this.state.isChargedTimeline) {
+              this.setState({
+              isChargedTimeline: true,
+            })
+            }
+            }}
           />
+
         </div>
       </div>
     )
