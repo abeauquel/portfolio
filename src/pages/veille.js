e789fdacb513c9f052954794512b38df483984a7
Alexandre Beauquel <31766247+abeauquel@users.noreply.github.com>
2018-11-02 09:34:15 -0400

    Feat : lien direct vers le twitter

--- src/pages/veille.js
+++ src/pages/veille.js
@@ -44,6 +44,9 @@ class Veille extends React.Component {
         {this.state.isChargedTimeline ? null:
           <div style={{textAlign:'center'}}>
             <p style={{textAlign:'left', paddingBottom:'15%'}}>Chargement de la timeline twitter</p>
+            <p style={{textAlign:'left', paddingBottom:'15%'}}>
+              Si la timeline ne charge pas : <a href={'https://twitter.com/alexbeauquel?lang=fr' target="_blank" rel="noopener noreferrer"}> Lien direct vers twitter</a>
+            </p>
             <Loader
               type="Plane"
               color="#00BFFF"
